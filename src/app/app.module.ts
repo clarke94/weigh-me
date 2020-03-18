c48437c7e034178df42a86ade8becad0123e1b1f
Liam Clarke <liam.clarke.94@outlook.com>
2020-03-18 19:19:13 +0000

    Add Weight component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,35 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { WeightComponent } from './components/weight/weight.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavBarComponent,
-        ProfileComponent
+        ProfileComponent,
+        WeightComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
+        ReactiveFormsModule,
         MatToolbarModule,
         MatButtonModule,
+        MatInputModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
         ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
     ],
     providers: [],
