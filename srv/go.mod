d96d46ef557282bb9caee17a1394911e73058821
Liam Clarke <liam.clarke.94@outlook.com>
2020-03-26 16:59:52 +0000

    Add Postgres Connection

--- srv/go.mod
+++ srv/go.mod
@@ -8,6 +8,6 @@ require (
 	github.com/go-pg/pg v8.0.6+incompatible
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/joho/godotenv v1.3.0
-	github.com/lib/pq v1.3.0 // indirect
+	github.com/lib/pq v1.3.0
 	mellium.im/sasl v0.2.1 // indirect
 )
--- srv/internal/driver/connect.go
+++ srv/internal/driver/connect.go
@@ -1,47 +1,44 @@
 package driver
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	_ "github.com/lib/pq" // postgres golang driver
 )
 
-// DB name
-const dbName = "weigh-me-cluster"
-
-// Collection name
-const collectionName = "weight"
-
-// Collection object
-var collection *mongo.Collection
-
 // Connect will connect to the mongo database
-func Connect() (*mongo.Collection, error) {
-	godotenv.Load()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	connectString := fmt.Sprintf("mongodb+srv://%s:%s@%s", os.Getenv("MONGO_USER"), os.Getenv("MONGO_PASSWORD"), os.Getenv("MONGO_CLUSTER"))
-
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectString))
+func Connect() (*sql.DB, error) {
+	// Get Environment variables
+	err := godotenv.Load(".env")
 	if err != nil {
 		return nil, err
 	}
 
-	err = client.Ping(ctx, nil)
+	// open the connection
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		return nil, err
 	}
 
-	log.Print("Connected to MongoDB")
-	collection = client.Database(dbName).Collection(collectionName)
+	// check the connection
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
 
-	return collection, nil
+	log.Print("Successfully connected")
+	// return the connection
+	return db, nil
 }
