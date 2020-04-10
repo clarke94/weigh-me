fe29244ee87d3e0e52cb66301df28e9ee88aeb60
Liam Clarke <liam.clarke.94@outlook.com>
2020-04-10 18:31:02 +0100

    Add GetAllWeights serverless endpoint

---
+++ srv/endpoints/weights/getall/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/clarke94/weigh-me/srv/internal/driver"
+)
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(request events.APIGatewayProxyRequest) (Response, error) {
+	var buf bytes.Buffer
+
+	// call the GetAllWeights function ti retrieve all weights
+	weights, err := driver.GetAllWeights()
+	if err != nil {
+		return Response{StatusCode: 404, Body: err.Error()}, err
+	}
+
+	body, err := json.Marshal(weights)
+	if err != nil {
+		return Response{StatusCode: 404, Body: err.Error()}, err
+	}
+
+	json.HTMLEscape(&buf, body)
+
+	resp := Response{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            buf.String(),
+		Headers: map[string]string{
+			"Content-Type":                "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+	}
+
+	return resp, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
