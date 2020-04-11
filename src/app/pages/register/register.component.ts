4670fd53b965d54df8306604ac8578846f1d03f3
Liam Clarke <liam.clarke.94@outlook.com>
2020-04-11 19:19:51 +0100

    Add personal information register step

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+    personalInfoForm: FormGroup;
+    fitnessForm: FormGroup;
 
-  constructor() { }
+    constructor(
+        private formBuilder: FormBuilder
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.InitialisePersonalInfoForm();
+        this.InitialiseFitnessForm();
+    }
 
+    InitialisePersonalInfoForm(): void {
+        this.personalInfoForm = this.formBuilder.group({
+            displayName: ['', Validators.compose([
+                Validators.required,
+                Validators.minLength(3),
+            ])],
+            gender: ['', Validators.compose([
+                Validators.required,
+                Validators.minLength(3),
+            ])],
+            dob: ['', Validators.required]
+        });
+    }
+
+    InitialiseFitnessForm(): void {
+        this.fitnessForm = this.formBuilder.group({
+            displayName: ['', Validators.compose([
+                Validators.required,
+                Validators.minLength(3),
+            ])],
+            gender: ['', Validators.compose([
+                Validators.required,
+                Validators.minLength(3),
+            ])]
+        });
+    }
 }
--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -3,13 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
-
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-    declarations: [RegisterComponent],
+    declarations: [
+        RegisterComponent
+    ],
     imports: [
         CommonModule,
-        RegisterRoutingModule
+        RegisterRoutingModule,
+        MatStepperModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ]
 })
 export class RegisterModule { }
